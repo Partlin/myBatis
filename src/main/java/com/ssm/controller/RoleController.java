3e7d0734811497bf2cb8afc4f2687694c79a9b48
lin <lin@DESKTOP-J3D3492>
2018-01-15 14:28:43 +0800

    add httpServletRequest 获取请求参数

--- src/main/java/com/ssm/controller/RoleController.java
+++ src/main/java/com/ssm/controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.ssm.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssm.model.Role;
 import com.ssm.service.RoleService;
@@ -17,6 +19,10 @@ public class RoleController {
 	@Autowired
 	private RoleService roleService;
 	
+	/**
+	 * 通过@RequestParam获取请求参数
+	 * 
+	 */
 	@RequestMapping("/RoleInfo")
 	public void RoleInfo(@RequestParam("id") int id,HttpServletResponse response){
 		Role data= new Role();
@@ -24,7 +30,29 @@ public class RoleController {
 		String jsondata=JsonUtil.toJSONString(data);
 		JsonUtil.sendjson(jsondata, response);
 	}
-	
+	/**
+	 * 通过HttpServletRequest获取请求参数
+	 * 
+	 */
+	@RequestMapping("/RoleInfo1")
+	public void RoleInfo1(HttpServletRequest request,HttpServletResponse response){
+		int id = Integer.parseInt(request.getParameter("id"));
+		Role data= new Role();
+		data = roleService.selectByPrimaryKey(id);
+		String jsondata=JsonUtil.toJSONString(data);
+		JsonUtil.sendjson(jsondata, response);
+	}
+	/**
+	 * 通过@ResponseBody返回JSON数据
+	 * 
+	 */
+	@RequestMapping("/RoleInfo2")
+	public @ResponseBody Role RoleInfo2(HttpServletRequest request,HttpServletResponse response){
+		int id = Integer.parseInt(request.getParameter("id"));
+		Role data= new Role();
+		data = roleService.selectByPrimaryKey(id);
+		return data;
+	}
 	@RequestMapping("/RoleInsert")
 	public void RoleInsert(@RequestParam("id") int id,@RequestParam("roleName") String roleName,
 			@RequestParam("note") String note,HttpServletResponse response){
